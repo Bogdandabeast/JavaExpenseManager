54c3c3fdfbc22954b24147b3aae357f180f10a71
Fran <francisco.sanchezchapman@iesjuandeherrera.net>
2024-04-26 21:02:24 +0200

    instancia de clases

--- src/ExpenseManager.java
+++ src/ExpenseManager.java
@@ -1,5 +1,25 @@
-// This class manages a list of Expense objects and includes methods to add,
-// list, and summarize expenses, as well as to validate inputs and handle file data.
+import java.util.Map;
+
 public class ExpenseManager {
-    
+    private Map<String, Expense> expenses;
+
+    public ExpenseManager(Map<String, Expense> expenses) {
+        this.expenses = expenses;
+    }
+
+    // Método para listar los gastos
+    public void listExpenses() {
+        for (String category : expenses.keySet()) {
+            System.out.println("Categoría: " + category);
+            expenses.get(category).displayAllExpenses();
+        }
+    }
+
+    // Método para resumir los gastos
+    public void summarizeExpenses() {
+        for (String category : expenses.keySet()) {
+            double totalExpense = expenses.get(category).getTotalExpense(category);
+            System.out.println("Categoría: " + category + ", Gasto total: " + totalExpense);
+        }
+    }
 }
